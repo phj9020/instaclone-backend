db70d06c04f2471efed2f9e3cd68469a31815984
HanJin Park <phj9020@hotmail.com>
2021-05-11 15:29:06 -0400

    See Profile, createAccount func added

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-// node module에 있는 프리즈마 클라이언트 생성
+// initialize client
 const client = new PrismaClient()
 
 
---
+++ users/users.mutations.js
@@ -0,0 +1,46 @@
+import bcrypt from 'bcrypt';
+import client from "../client";
+
+export default {
+    Mutation: {
+        createAccount: async(_, {firstName, lastName, username, email, password}) =>  {  
+            try {
+                // check @unique (uesrname, email) from prisma already exist 
+                const existingUser = await client.user.findFirst({
+                    where: {
+                        OR: [
+                            {
+                                username: username
+                            },
+                            {
+                                email: email
+                            },
+                        ],
+                    },
+                });
+        
+                if(existingUser) {
+                    throw new Error("This uesrname/email is already in use.");
+                }
+                
+                // hash password 
+                const hashedPassword = await bcrypt.hash(password, 10);
+                
+                // save and return user
+                return client.user.create({
+                    data : {
+                        username: username,
+                        email: email,
+                        firstName: firstName,
+                        lastName: lastName,
+                        password: hashedPassword
+                    }
+                })
+
+            } catch(error) {
+                return error.message;
+            }
+            
+        },
+    }
+}
