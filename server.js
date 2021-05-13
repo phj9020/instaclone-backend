4ced0a1ed6265dc1c9b2a4acc9b571d7bda6ad67
HanJin Park <phj9020@hotmail.com>
2021-05-13 11:47:11 -0400

    switch apollo-server to express &  apollo-server-express

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
-import { ApolloServer } from 'apollo-server';
+import express from "express";
+import logger from 'morgan';
+import { ApolloServer } from 'apollo-server-express';
 import {typeDefs, resolvers} from './schema';
 import {getUser} from "./users/users.utils";
+
+const PORT = process.env.PORT;
 
 // Create Server
 const server = new ApolloServer({
@@ -14,8 +18,11 @@ const server = new ApolloServer({
     }
 });
 
-const PORT = process.env.PORT;
+// tell apollo server work with express server
+const app = express();
+app.use(logger("tiny"));
+server.applyMiddleware({ app });
 
-server.listen(PORT).then(() => {
+app.listen({ port: PORT }, ()=> {
     console.log(`🚀  Server ready at http://localhost:${PORT}`);
 });
