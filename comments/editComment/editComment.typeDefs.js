447c8259dfb62221b8191b1645a58aa497d530a0
HanJin Park <phj9020@hotmail.com>
2021-05-19 13:50:08 -0400

    created MutationResponse in shared/shared.typeDefs.js & replace all MutationResult with MutationResponse

--- comments/editComment/editComment.typeDefs.js
+++ comments/editComment/editComment.typeDefs.js
@@ -1,12 +1,8 @@
 import {gql} from "apollo-server-express";
 
 export default gql`
-    type EditCommentResult {
-        ok: Boolean!
-        error: String
-    }
     type Mutation {
-        editComment(id:Int!, payload:String!): EditCommentResult!
+        editComment(id:Int!, payload:String!): MutationResponse!
     }
 
 `
--- users/followUser/followUser.typeDefs.js
+++ users/followUser/followUser.typeDefs.js
@@ -1,13 +1,9 @@
 import {gql} from "apollo-server-express";
 
 export default gql`
-    type FollowUserResult {
-        ok: Boolean!,
-        error: String
-    }
     type Mutation {
         followUser(
             username: String!,
-        ): FollowUserResult!
+        ): MutationResponse!
     }
 `
