006677208802d0e89513bbd46c8667266f315030
HanJin Park <phj9020@hotmail.com>
2021-05-19 14:44:22 -0400

    protectedResolver Refactor in order to protect Query : rif not logged in return null

--- users/users.utils.js
+++ users/users.utils.js
@@ -24,9 +24,17 @@ export const getUser = async(token)=> {
 export const protectResolver = (ourResolver) => (root, args, context, info) => {
     // if not logged in return below object
     if(!context.loggedInUser) {
-        return {
-            ok: false,
-            error: "You must be logged in"
+        // 유저가 query 하는지 mutation하는지 체크
+        console.log(info)
+        const query = info.operation.operation === "query";
+        if(query) {
+            return null;
+        } else {
+            return {
+                ok: false,
+                error: "You must be logged in"
+            }
+
         }
     }
     // if logged in return resolver
